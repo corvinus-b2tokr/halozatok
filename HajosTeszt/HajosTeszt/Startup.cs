f6f3b455bba4ea082345001f83986cc8c596d71e
Olivér Zoltán Karsai <oliver.karsai@stud.uni-corvinus.hu>
2021-04-15 12:52:16 +0200

    8. hét feladata kész

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +31,7 @@ namespace HajosTeszt
 
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("hajo.html");
+            options.DefaultFileNames.Add("kerdesek.html");
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
 
@@ -43,6 +44,11 @@ namespace HajosTeszt
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
